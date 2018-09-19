68b3bfe4b2f75486c36a282de440e2fc0b162149
Ahmed <omar0121@umn.edu>
2018-09-19 11:03:24 -0500

    edited the homepage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link  } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Nav from '../../components/Nav/Nav';
 
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 import { USER_ACTIONS } from '../../redux/actions/userActions';
 import { triggerLogout } from '../../redux/actions/loginActions';
@@ -34,57 +39,124 @@ class HomePage extends Component {
       content = (
         <div>
 
-       <h2>
+          <h2>
             Welcome, {this.props.user.userName}hmed!
             </h2>
-          <br/>
+          <br />
+
+          <div>
+            <h2>
+              Stargazers is an application that provides you with a means to
 
 
-<br/>
-<br/>
-<br/>
+  </h2>
+          </div>
 
-<div>
-  <p>
-    Stargazers is an application that allows users to yada yada..... 
+          <p> static information about observatories - 4 posts GOES HERE!!</p>
+          <div className="bortle-scale-info">
+            <p> The Bortle scale is a means to measure the brightness of the sky at night using a
+  <br />
+              numerical identifier that each provide a description of the sky quality.
+  <br />
+            </p>
+          </div>
 
-  </p>
-</div>
+          <br />
 
-<p>
-  -------------
+          <div className="NELM-info">
+            <p>The NELM index (Naked-Eye Limiting Magnitude) is the identifying marker that allows
+        <br />
+              stargazers to comapare the relative darkness of their current location to a more structured scale
+        <br />
+              using their eyes alone.
+            </p>
+
+          </div>
+
+          <div className="table-intro"> Below is a table with detail on how the scaling works:</div>
+          <br />
+          <Table color="secondary">
+            {/* TABLE HEADER INFORMATION */}
+
+            <TableHead>
+              <TableCell>Bortle Scale value:</TableCell>
+              <TableCell>Description</TableCell>
+              <TableCell>NELM</TableCell>
+            </TableHead>
+
+
+            {/* TABLE BODY STARTS HERE */}
+            <TableBody>
+              <TableRow>
+                <TableCell><h1>1</h1></TableCell>
+                <TableCell>Excellent Dark-Sky Site </TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><h1>2</h1></TableCell>
+                <TableCell>Typical Truly Dark Site</TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><h1>3</h1></TableCell>
+                <TableCell>Rural Sky</TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><h1>4</h1></TableCell>
+                <TableCell>Rural-Subarban Transitory Site</TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><h1>5</h1></TableCell>
+                <TableCell>Bright Suburban Sky</TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><h1>6</h1></TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><h1>7</h1></TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><h1>8</h1></TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><h1>9</h1></TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+
+          <br />
+          <br />
+          <br />
+
+          <p>
+            -------------
 </p>
 
-<p> static information about observatories - 4 posts
-</p>
+          <p>user posts render here</p>
 
-<p>
-  -------------
-</p>
+          <p>
 
-
-<br/>
-<br/>
-<br/>
-
-<p>
-  -------------
-</p>
-
-<p>user posts render here</p>
-
-<p>
-
-</p>
-<br/>
-<br/>
-<Link to="/location">Create New Post</Link>
+          </p>
+          <br />
+          <br />
+          <Link to="/location">Create New Post</Link>
 
 
 
 
 
-<button onClick={this.logout} > Sign Out </button>
+          <button onClick={this.logout} > Sign Out </button>
         </div>
       );
     }
