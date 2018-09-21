d9254d096e108e524e89b9941ab66cf9792f8d64
Ahmed <omar0121@umn.edu>
2018-09-20 23:17:48 -0500

    style changes w/material

--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom';
 import Nav from '../../components/Nav/Nav';
 import { USER_ACTIONS } from '../../redux/actions/userActions';
 import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Dialog from '@material-ui/core/Dialog';
+
 
 
 const mapStateToProps = state => ({
@@ -52,46 +55,28 @@ class ProfilePage extends Component {
               return (
 
                 <div key={post.id}>
+                <Grid container spacing={8}></Grid>
+                <Grid item xs={4}>
                   <Paper >
                     <div className="card">
                       <span>
-                        <p>Name of location:</p>
-                        <h4>{post.title}</h4>
-                        <p>Description:</p>
-                        <h4>{post.description}</h4>
-                        <h4><p>Bortle Scale Value:</p>{post.bortle_value}</h4>
+                        <p>Name of location:{post.title}</p>
+                        <p>Description:{post.description}</p>
+                        <p>Bortle Scale Value:{post.bortle_value}</p>
 
-                        <img src={post.image_path} alt={post.description} height="400px" width="400px" />
+                        <img src={post.image_path} alt={post.description} height="200px" width="200px" />
                       </span>
                     </div>
 
 
                   </Paper>
+                  </Grid>
+                  <br/>
+                  
                   <br />
                 </div>
               )
             })}
-
-
-
-
-
-            <Paper>
-              <div className="card">
-                user posts here....
-                    </div>
-            </Paper>
-
-            <p> pagination of user information also here..</p>
-            <p> 1 , 2 , 3 , 4</p>
-
-
-            <br />
-            <br />
-
-
-            <div> link to create new post page here... </div>
-
             <Link to="/location">Create New Post</Link>
 
 
